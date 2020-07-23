f4a9b192177d51a214d7d517c08ebd6e3af65bdc
csanders891 <65356624+csanders891@users.noreply.github.com>
2020-07-23 00:10:15 -0500

    Add files via upload

---
+++ script/infinite_loop.js
@@ -0,0 +1,7 @@
+var x=48;
+var string="Gadeo";
+
+while (x > 36)
+{
+alert(string);
+}                                                                                                                                                                                                                               
